b6da0c243ce6c123b8aa96aa4e5067dc01b349a4
Pablo Barrenechea <pbarrenechea@sparkdigital.com>
2017-12-11 22:57:21 -0300

    added download directory

--- src/file-downloads.js
+++ src/file-downloads.js
@@ -17,7 +17,7 @@ function copyFile(src, dest) {
 }
 
 // Path where we store the download sessions
-const DL_SESSION_FOLDER = './static/download';
+const DL_SESSION_FOLDER = __dirname + '/static/download';
 
 export default class DownloadHandler {
 /* Creates a download session */
